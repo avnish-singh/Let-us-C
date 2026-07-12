a6f192190692084ed5d2e5fe13a20808b98debee
Avnish Singh <70936958+avnish-singh@users.noreply.github.com>
2026-07-12 14:12:50 +0530

    Pre Post Increment Decrement Operators

--- ComplexDecisionMaking.c
+++ ComplexDecisionMaking.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int i = 4, j = -1, k=0,w,x,y,z;
-    w = i || j || k;
-    x = i && j && k;
-    y = i || j && k;
-    z = i && j || k;
-    printf("w = %d x = %d y = %d z = %d\n",w,x,y,z);
+    int i=-4,j,num;
+    if(num < 0 ? 0 : num*num)
+        printf("num %d\n", num);
+        printf("%d\n",j);
     return 0;
 }
--- Untitled1.c
+++ Untitled1.c
@@ -2,12 +2,23 @@
 #include<stdio.h>
 
 int main(){
+
+    while('a' < 'b')
+        printf("Malayalam is palindrome\n");
+
+    /*int x=4, y=3, z;
+    z = x-- - y;
+    printf(" x :%d\n y :%d\n z :%d\n", x, y, z);
+    */
+
+
+/*
     char g;
     int yos,qual,sal = 0;
     printf("Enter Gender, Year of Service, Qualification ( 0 = G, 1 = PG ):");
     scanf("%c %d %d", &g,&yos, &qual);
 
-/*    if(g=='M' && yos>=10 && qual==1)
+    if(g=='M' && yos>=10 && qual==1)
     {
         sal = 15000;
         printf("Male PG >=10Y of service Salary : %d", sal);
@@ -27,7 +38,7 @@ int main(){
         printf("Post Graduate Female under 10Y of service Salary : %d", sal = 10000);
     else if(g=='F' && yos<10 && qual==0)
         printf("Graduate Female under 10Y of service Salary : %d", sal = 6000);
-*/
+
 
     if(g=='M' && yos>=10 && qual==1)
         sal = 15000;
@@ -44,5 +55,6 @@ int main(){
     else if(g=='F' && yos<10 && qual==0)
         sal = 6000;
     printf("Salary : %d", sal);
+*/
     return 0;
 }
